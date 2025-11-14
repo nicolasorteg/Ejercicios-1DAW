e7beee947f8e8af7db5ddf40ef3e00db80c86253
nicolasorteg <nicolasortegaf22@gmail.com>
2025-11-14 18:07:05 +0100

    enum del menu update hecho

---
+++ 06-Gestion Empleados/Empleados/Enums/MenuUpdate.cs
@@ -0,0 +1,11 @@
+﻿namespace Empleados.Enums;
+
+// almacena las opciones para el menu de actualizar empleado
+internal enum MenuUpdate {
+    Salir = 0,
+    Nip = 1,
+    Nombre = 2,
+    Edad = 3,
+    Email = 4,
+    Cargo = 5
+}
