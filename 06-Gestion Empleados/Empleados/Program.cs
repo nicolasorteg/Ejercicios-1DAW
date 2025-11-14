1f2acf18983bd553eb822742210c2c9fd0f15abb
nicolasorteg <nicolasortegaf22@gmail.com>
2025-11-14 18:44:10 +0100

    comienzo con la asignacion de datos

--- 06-Gestion Empleados/Empleados/Program.cs
+++ 06-Gestion Empleados/Empleados/Program.cs
@@ -1,6 +1,7 @@
 ﻿// ._.
 
 using System.Text;
+using Empleados.Enums;
 using Serilog;
 using Empleados.Structs;
 
@@ -34,7 +35,31 @@ void Main(string[] args) {
 
     int numEmpleados = 0;
 
-    Empleado?[] plantilla= new Empleado?[EmpleadosIniciales];
-    //AsignarDatos(plantilla, ref numEmpleados);
+    Empleado?[] plantilla= new Empleado?[TamañoInicial];
+    AsignarDatos(plantilla, ref numEmpleados);
+
+}
+
+// --------------------------- FUNCIONES AUXILIARES
+
+/*
+ * Se encarga de asignar X valores iniciales a la plantilla para así
+ * iniciar la gestión con datos ejemplificativos
+ */
+void AsignarDatos(Empleado?[] plantilla, ref int empleados) {
+
+    // creamos 5 empleados con datos inventados
+    var e1 = new Empleado { Nip = "AA1", nombre = "José Luis", Edad = 18, Email = "joseluisgs@gmail.com", Cargo = Cargo.Director };
+    var e2 = new Empleado { Nip = "BB2", nombre = "Rafa", Edad = 20, Email = "rafa123@gmail.com", Cargo = Cargo.Operario };
+    var e3 = new Empleado { Nip = "CC3", nombre = "Aitor", Edad = 40, Email = "aitorcrack69@gmail.com", Cargo = Cargo.Operario };
+    var e4 = new Empleado { Nip = "DD4", nombre = "Nicolás", Edad = 20, Email = "nicolas33@gmail.com", Cargo = Cargo.Supervisor };
+    var e5 = new Empleado { Nip = "EE5", nombre = "Carlos", Edad = 25, Email = "carlitosalcaraz@gmail.com", Cargo = Cargo.Tecnico };
+    
+    // sorteamos su posición en la plantilla
+    int posicion = 0;
+
+    while (empleados < EmpleadosIniciales) {
+        posicion = random.Next(plantilla.Length); // de 0-9 en este caso
+    }
 
 }
