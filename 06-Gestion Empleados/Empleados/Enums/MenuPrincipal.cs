4d1a1a61c72770707cf301c77f96e752772a87d5
nicolasorteg <nicolasortegaf22@gmail.com>
2025-11-14 18:03:13 +0100

    enum del menu principal crud

---
+++ 06-Gestion Empleados/Empleados/Enums/MenuPrincipal.cs
@@ -0,0 +1,11 @@
+﻿namespace Empleados.Enums;
+
+internal enum MenuPrincipal {
+    Salir = 0,              // ._.
+    CrearEmpleado = 1,      // C
+    VerEmpleados = 2,       // R
+    ListarEmpleadosNip = 3, // R
+    MostrarPorCargo = 4,    // R
+    ActualizarEmpleado = 5, // U
+    BorrarEmpleado = 6,     // D
+}
