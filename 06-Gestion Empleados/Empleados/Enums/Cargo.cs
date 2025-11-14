e6db9da3a15acf18ce5c9801e01e69885df3ae35
nicolasorteg <nicolasortegaf22@gmail.com>
2025-11-14 17:59:27 +0100

    añado el dato cargo usando un enum

---
+++ 06-Gestion Empleados/Empleados/Enums/Cargo.cs
@@ -0,0 +1,11 @@
+﻿namespace Empleados.Enums;
+
+// almacena los tipos de cargo que puede tener un empleado
+internal enum Cargo {
+    Director,
+    Subdirector,
+    Manager,
+    Tecnico,
+    Supervisor,
+    Operario
+}
--- 06-Gestion Empleados/Empleados/Structs/Empleado.cs
+++ 06-Gestion Empleados/Empleados/Structs/Empleado.cs
@@ -1,8 +1,10 @@
 ﻿namespace Empleados.Structs;
+using Empleados.Enums;
 
 internal struct Empleado {
     public string Nip;
     public string nombre;
     public int Edad;
     public string Email;
+    public Cargo cargo;
 }
