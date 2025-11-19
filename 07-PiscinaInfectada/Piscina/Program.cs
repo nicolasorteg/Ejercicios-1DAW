ec3da4aadc05969fff7fd538a43f8761548e62a9
nicolasorteg <nicolasortegaf22@gmail.com>
2025-11-19 17:55:02 +0100

    init proyeto (paquetes instalados)

---
+++ 07-PiscinaInfectada/Piscina/Program.cs
@@ -0,0 +1,26 @@
+﻿// ._.
+
+using System.Globalization;
+using System.Text;
+using Serilog;
+using static System.Console;
+
+// zona de constantes
+
+
+var random = Random.Shared;
+Log.Logger = new LoggerConfiguration().WriteTo.File("logs/log.txt").MinimumLevel.Debug().CreateLogger();
+var localeEs = new CultureInfo("es-ES");
+Title = "Piscina Infectada";
+OutputEncoding = Encoding.UTF8;
+Clear();
+
+Main(args);
+
+WriteLine("\n👋 Presiona una tecla para salir...");
+ReadKey();
+return;
+
+void Main(string[] args) {
+    Console.WriteLine("Hola Caracola");
+}
