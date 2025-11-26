64dbdc53becb4f875d80a7167ec7f68fcce03438
nicolasorteg <nicolasortegaf22@gmail.com>
2025-11-26 04:44:30 +0100

    añade icono aplicacion

--- Prog. Orientada a Objetos/01-Gestion Equipo Futbol/Equipo Futbol/obj/Debug/net10.0/Equipo Futbol.AssemblyInfo.cs
+++ Prog. Orientada a Objetos/01-Gestion Equipo Futbol/Equipo Futbol/obj/Debug/net10.0/Equipo Futbol.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Equipo Futbol")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af4557e3557587bebc7057a2f48fbd1318445a21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aecbf11b98e1d4182bd1ac7a6f080c150bc2b24")]
 [assembly: System.Reflection.AssemblyProductAttribute("Equipo Futbol")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Equipo Futbol")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
