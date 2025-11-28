ee92d92c166a1a04fdb67caed4dc55595b91c317
nicolasorteg <nicolasortegaf22@gmail.com>
2025-11-28 06:11:37 +0100

    commit para merge gest futbol

--- Prog. Orientada a Objetos/01-Gestion Equipo Futbol/Equipo Futbol/obj/Debug/net10.0/Equipo Futbol.AssemblyInfo.cs
+++ Prog. Orientada a Objetos/01-Gestion Equipo Futbol/Equipo Futbol/obj/Debug/net10.0/Equipo Futbol.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Equipo Futbol")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f8fa01c7eebdbfd1c3cffc2fef71765dacdda93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25d23fe6ad5c44896d0bacc051dd89161463c7d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Equipo Futbol")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Equipo Futbol")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
