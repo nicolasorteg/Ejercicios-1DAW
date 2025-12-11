30a370929fa46fd959f3d2a8405bde2a25780c5b
nicolasorteg <nicolasortegaf22@gmail.com>
2025-12-11 01:03:32 +0100

    images añadidas

--- Prog. Orientada a Objetos/04-Gestion Banda Rock/Gestion/Program.cs
+++ Prog. Orientada a Objetos/04-Gestion Banda Rock/Gestion/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using Serilog;
 
-Console.WriteLine("Hello, World!");
+// daw's template
+Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File("Logs/log.log", rollingInterval: RollingInterval.Day).CreateLogger();
+Console.Title = "Gestión Banda Rock";
+Console.OutputEncoding = Encoding.UTF8;
+Console.Clear();
+Main();
+Console.WriteLine("\n👋 Presiona una tecla para salir...");
+Console.ReadKey();
+return;
+
+void Main() {
+    Console.WriteLine();
+}
