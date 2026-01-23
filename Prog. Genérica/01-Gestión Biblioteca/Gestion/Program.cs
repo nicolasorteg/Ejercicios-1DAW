72ae25f11e33ec20cc40ef27648c7c6d3b8e63a5
nicolasorteg <nicolasortegaf22@gmail.com>
2026-01-23 18:01:27 +0100

    init ejercicio

---
+++ Prog. Genérica/01-Gestión Biblioteca/Gestion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using Serilog;
+
+// daw's template
+Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File("Logs/log.log", rollingInterval: RollingInterval.Day).CreateLogger();
+Title = "Gestión Biblioteca";
+OutputEncoding = Encoding.UTF8;
+Clear();
+Main();
+WriteLine("\n👋 Presiona una tecla para salir...");
+ReadKey();
+return;
+
+void Main() {
+    
+}
