77d018537a0d269448ab1e97da020fab4f3f3b58
nicolasorteg <nicolasortegaf22@gmail.com>
2026-01-26 14:20:15 +0100

    enums acabados

--- Prog. Genérica/01-Gestión Biblioteca/Gestion/Enums/OpcionMenuPrincipal.cs
+++ Prog. Genérica/01-Gestión Biblioteca/Gestion/Enums/OpcionMenuPrincipal.cs
@@ -1,3 +1,12 @@
 ﻿namespace Gestion.Enums;
 
 public enum OpcionMenuPrincipal {
+    Salir,
+    VerTodos,
+    ListarEstado,
+    BuscarId,
+    OrdenarPaginas, // libros
+    Crear,
+    Actualizar,
+    Eliminar
+}
