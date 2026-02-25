18c8c99bf06a9a288acc428f20df720a181dfc9c
nicolasorteg <nicolasortegaf22@gmail.com>
2026-02-25 16:05:02 +0100

    add validator

---
+++ Prog. Ficheros/01-Comando Dir/Dir/Validators/ArgsValidator.cs
@@ -0,0 +1,29 @@
+﻿namespace Dir.Validators;
+
+/// <summary>
+/// Validador para los datos introducidos por argumento.
+/// </summary>
+public class ArgsValidator {
+    public IEnumerable<string> Validar(string[] args) {
+        var errores = new List<string>();
+
+        // valida que al menos exista un argumento
+        if (args.Length == 0) {
+            errores.Add("No se han proporcionado argumentos. Debe indicar al menos una ruta de directorio.");
+            return errores;
+        }
+
+        // valida que la ruta de origen sea válida y exista
+        if (string.IsNullOrWhiteSpace(args[0])) 
+            errores.Add("La ruta del directorio no puede estar vacía.");
+        else if (!Directory.Exists(args[0])) 
+            errores.Add($"El directorio origen '{args[0]}' no existe o no es accesible.");
+        
+        // valida la ruta de salida si se pasa
+        if (args.Length > 1) {
+            if (string.IsNullOrWhiteSpace(args[1])) 
+                errores.Add("El nombre del archivo de salida no puede estar vacío si se proporciona el argumento.");
+        }
+        return errores;
+    }
+}
