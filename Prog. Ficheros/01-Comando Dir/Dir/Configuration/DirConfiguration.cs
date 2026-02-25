9a91fd80cadaad0e2244a120a8e2a745edc22d7a
nicolasorteg <nicolasortegaf22@gmail.com>
2026-02-25 16:10:52 +0100

    update config

--- Prog. Ficheros/01-Comando Dir/Dir/Configuration/DirConfiguration.cs
+++ Prog. Ficheros/01-Comando Dir/Dir/Configuration/DirConfiguration.cs
@@ -8,4 +8,15 @@
 public static class DirConfiguration {
     public static string DirectoryPath { get; set; } = string.Empty;
     public static string FileOutputPath { get; set; } = string.Empty;
+    
+    /// <summary>
+    /// Método para almacenar los datos pasados por argumento en la configuración.
+    /// </summary>
+    /// <param name="strings"></param>
+    public static void MapearArgumentosAConfig(string[] strings) {
+        DirectoryPath = Path.GetFullPath(strings[0]);
+        if (strings.Length > 1) {
+            FileOutputPath = strings[1];
+        }
+    }
 }
--- Prog. Ficheros/01-Comando Dir/Dir/Program.cs
+++ Prog. Ficheros/01-Comando Dir/Dir/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Dir.Configuration;
+using Dir.Validators;
+
+Console.WriteLine("Iniciando comando Dir con Validación...");
+
+// instancia validator
+var validator = new ArgsValidator();
+var errores = validator.Validar(args);
+
+// comprobacion errores
+if (errores.Any()) {
+    Console.WriteLine("Se han encontrado errores de validación:");
+    foreach (var error in errores) Console.WriteLine($"- {error}");
+    return; 
+}
+
+// mapea los datos a config
+DirConfiguration.MapearArgumentosAConfig(args);
+
+// comando
+DirService.Run();
+return;
