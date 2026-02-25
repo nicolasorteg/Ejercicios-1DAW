66ef302c535aef1c229a8dfc5d81196961a61133
nicolasorteg <nicolasortegaf22@gmail.com>
2026-02-25 15:54:45 +0100

    add config

---
+++ Prog. Ficheros/01-Comando Dir/Dir/Configuration/DirConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace Dir.Configuration;
+
+/// <summary>
+/// Clase estática donde se almacena:
+/// - La ruta donde se ejecutará el comando.
+/// - La ruta del archivo donde se almacenará la información de la salida del comando.
+/// </summary>
+public static class DirConfiguration {
+    public static string DirectoryPath { get; set; } = string.Empty;
+    public static string FileOutputPath { get; set; } = string.Empty;
+}
