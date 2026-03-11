95152d0642a4746400e1212fd4d4cf18e5a369bd
nicolasorteg <nicolasortegaf22@gmail.com>
2026-03-11 16:00:14 +0100

    init ejercicio

---
+++ Prog. Ficheros/02-Pokemon/Pokemon/Program.cs
@@ -0,0 +1,14 @@
+﻿// daw's template
+
+using System.Text;
+
+Title = "Gestión Empresa Héroes";
+OutputEncoding = Encoding.UTF8;
+Clear();
+Main();
+WriteLine("\n👋 Presiona una tecla para salir...");
+ReadKey();
+return;
+
+void Main() {
+}
